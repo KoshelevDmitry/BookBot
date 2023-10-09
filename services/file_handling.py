ba0e03a3ff6b441ee9a1c30642944e34496991e7
KoshelevDmitry <sbsystem@inbox.ru>
2023-10-09 14:34:02 +0500

    Update edited files

--- services/file_handling.py
+++ services/file_handling.py
@@ -8,13 +8,38 @@ book: dict[int, str] = {}
 
 
 # Функция, возвращающая строку с текстом страницы и ее размер
-def _get_part_text(text: str, start: int, size: int):
-    pass
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+    slice = text[start: start + size + 1]
+    list = []
+
+    for g in range(3):
+        if (start + size) >= len(text) or slice[len(slice)-1] == ' ':
+            break
+        elif slice[len(slice)-1] in [',', '.', '!', ':', ';', '?']:
+            slice = slice[0: len(slice) - 1]
+        else:
+            break
+
+    for i in [',', '.', '!', ':', ';', '?']:
+        list.append(slice.rfind(i)+1)
+    slice_new = text[start: start + max(list)]
+
+    return slice_new, max(list)
 
 
 # Функция, формирующая словарь книги
 def prepare_book(path: str) -> None:
-    pass
+    file = open(path, encoding='utf-8')
+    text = file.read()
+    begin_position = 0
+    key = 1
+    while len(text) - begin_position > 0:
+        element_dict = _get_part_text(text, begin_position, PAGE_SIZE)
+        book[key] = element_dict[0].lstrip()
+        print(key, book[key])
+        key += 1
+
+        begin_position = begin_position + element_dict[1]
 
 
 # Вызов функции prepare_book для подготовки книги из текстового файла
